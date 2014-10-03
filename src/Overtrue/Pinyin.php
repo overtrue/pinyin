62f0b595f3bb3215e936ff39e4995e9dc586b6e2
joychao <anzhengchao@gmail.com>
2014-10-03 17:20:22 +0800

    修改算法，提高速度

--- src/Overtrue/Pinyin.php
+++ src/Overtrue/Pinyin.php
@@ -4,15 +4,13 @@ namespace  Overtrue;
 
 /**
  * Pinyin.php
- * 
- * @author Joy <anzhengchao@gmail.com>
+ *
+ * @author Carlos <anzhengchao@gmail.com>
  * @date   [2014-07-17 15:49]
  */
 
 /**
  * Chinese to pinyin translator
- *
- * @author Joy <anzhengchao@gmail.com>
  *
  * @example
  * <pre>
@@ -21,7 +19,7 @@ namespace  Overtrue;
  * </pre>
  */
 class Pinyin
-{   
+{
 
     /**
      * dictionary path
@@ -35,17 +33,10 @@ class Pinyin
      *
      * @var array
      */
-    protected static $setting = array(
+    protected $settings = array(
                                  'delimiter' => ' ',
                                  'accent'    => true,
                                 );
-
-    /**
-     * instance
-     *
-     * @var Pinyin
-     */
-    protected static $instance;
 
     /**
      * constructor
@@ -54,93 +45,136 @@ class Pinyin
      */
     public function __construct()
     {
-        ini_set('memory_limit', '160M');
         $this->dictionary = __DIR__ . '/cedict/cedict_ts.u8';
     }
 
     /**
-     * set the dictionary.
+     * set.
      *
-     * @param array $setting settings.
+     * @param array $settings settings.
      */
-    public static function set(array $setting = array())
+    public function set($key, $value)
     {
-        self::$setting = array_merge(self::$setting, $setting);
+        $this->settings[$key] = $value;
     }
 
     /**
-     * get Pinyin instance
+     * setting.
      *
-     * @return Pinyin
+     * @param array $settings settings.
      */
-    public static function getInstance()
+    public function setting(array $settings = array())
     {
-        if (is_null(self::$instance)) {
-            self::$instance = new self;
-        }
-
-        return self::$instance;
+        $this->settings = array_merge($this->settings, $setting);
     }
 
     /**
      * chinese to pinyin
      *
      * @param string $string  source string.
-     * @param array  $setting settings.
+     * @param array  $settings settings.
      *
      * @return string
      */
-    public static function trans($string, array $setting = array())
+    public function trans($string, array $settings = array())
     {
-        $instance = self::getInstance();
-
         // merge setting
-        empty($setting) || self::set($setting);
+        empty($setting) || $this->settings($setting);
 
-        if (!function_exists('pinyin')) {
-            error_log('转拼音未使用拓展(pinyin)');
-            $string = self::transByPHP($string);
-        } else {
-            $string = pinyin($string, ' ');
-        }
+        $string = $this->string2pinyin($string);
 
         // add accents
-        if(self::$setting['accent']) {
-            $string = $instance->pinyin_addaccents(strtolower($string));
+        if($this->settings['accent']) {
+            $string = $this->pinyin_addaccents(strtolower($string));
         } else {
-            $string = $instance->removeTone(strtolower($string));
+            $string = $this->removeTone(strtolower($string));
         }
 
         // clean the string
-        $string = $instance->removeUnwantedCharacters($string);
-        
-        // add delimiter
-        $string = $instance->addDelimiter($string);
+        $string = $this->removeUnwantedCharacters($string);
 
-        return $instance->escape($string);
+        // add delimiter
+        $string = $this->addDelimiter($string);
+
+        return $this->escape($string);
     }
 
     /**
-     * php代码去解析
+     * get first letters of chars
      *
-     * @param string $string 
+     * @param string $string    source string.
+     * @param string $delimiter delimiter for letters.
      *
      * @return string
      */
-    protected static function transByPHP($string)
+    public function firstLetter($string, $delimiter = ' ')
     {
-        $instance = self::getInstance();
-        $dictionary = $instance->loadDictionary();
-            
-        // do replace
-        foreach ($dictionary as $line) {
-            $string = str_replace($line['simplified'], "{$line['pinyin_marks']} ", $string);
-            if (!$instance->containsChinese($string)) {
-                break;
+        $this->set('delimiter', $delimiter);
+
+        $letters = [];
+
+        for ($i = 0; $char = $this->getChar($string, $i); $i++) {
+            if ($letter = $this->getCharFirstLetter($char)) {
+                $letters[] = $letter;
             }
         }
 
-        return $string;
+        return $this->addDelimiter(join(' ', $letters));
+    }
+
+    /**
+     * replace string to pinyin
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    protected function string2pinyin($string)
+    {
+        $stringLength = $this->getStringLength($string);
+        $dictionary   = $this->loadDictionary();
+
+        $pingyin = [];
+
+        // do replace
+        for ($i = 0; $i < $stringLength; ) {
+            $str = $this->getChar($string, $i);
+            $next = null;
+
+            while ($i < $stringLength
+                    && $next = $str . $this->getChar($string, ++$i)
+                    && !empty($dictionary[$next])
+                    && $str = $next) {};
+
+            $pingyin[] = isset($dictionary[$str]) ? $dictionary[$str] : $str;
+        }
+
+        return join(' ', $pingyin);
+    }
+
+    /**
+     * get char
+     *
+     * @param string  $string source string.
+     * @param integer $offset offset.
+     *
+     * @return string
+     */
+    protected function getChar($string, $offset)
+    {
+        return mb_substr($string, $offset, 1, 'UTF-8');
+    }
+
+    /**
+     * get length of string
+     *
+     * @param string $string source string.
+     *
+     * @return integer
+     */
+    protected function getStringLength($string)
+    {
+        return mb_strlen($string, 'UTF-8');
     }
 
     /**
@@ -183,42 +217,89 @@ class Pinyin
      *
      * @param string $dictionary path of dictionary file.
      *
-     * @return array 
+     * @return array
      */
     protected function parseDictionary($dictionary)
     {
-        //ini_set('memory_limit', '180M');
-        $dictionary = file($dictionary);
+        $handle = fopen($dictionary, 'r');
         $regex = "#(.*?) (.*?) \[(.*?)\] \/(.*)\/#";
 
         $content = array();
 
-        foreach ($dictionary as $entry) {
-            if (0 === stripos($entry, '#')) {
+        while ($line = fgets($handle, 4096)) {
+            if (0 === stripos($line, '#')) {
+                continue;
+            }
+            preg_match($regex, $line, $matches);
+
+            if (empty($matches[2]) || empty($matches[2])) {
                 continue;
             }
 
-            preg_match($regex, $entry, $matches);
-
-            $content[] = array(
-                          //'traditional'    => $matches[1],
-                          'simplified'     => $matches[2],
-                          //'pinyin_numbers' => $matches[3],
-                          'pinyin_marks'   => $matches[3],
-                          //'translation'    => $this->escape($matches[4]),
-                         );
+            $content[$matches[2]] = $matches[3];
+            // $content[$matches[2]] = array(
+            //               //'traditional'    => $matches[1],
+            //               'simplified'     => $matches[2],
+            //               //'pinyin_numbers' => $matches[3],
+            //               'pinyin_marks'   => $matches[3],
+            //               //'translation'    => $this->escape($matches[4]),
+            //              );
         }
 
-        // sort by simplified string length.
-        usort($content, function($a, $b){
-            if (mb_strlen($a['simplified']) == mb_strlen($b['simplified'])) {
-                return 0;
-            }
-
-            return mb_strlen($a['simplified']) < mb_strlen($b['simplified']) ? 1 : -1;
-        });
-
         return $content;
+    }
+
+    /**
+     * get first letter of char.
+     *
+     * @param string $string source string.
+     *
+     * @return string
+     */
+    protected function getCharFirstLetter($char)
+    {
+        if (empty($char)) {
+            return '';
+        }
+
+        $fchar = ord($char{0});
+
+        if ($fchar >= ord('A') && $fchar <= ord('z')) {
+            return strtoupper($char{0});
+        }
+
+        $s1 = iconv('UTF-8', 'gb2312', $char);
+        $s2 = iconv('gb2312', 'UTF-8', $s1);
+
+        $s = $s2 == $char ? $s1 : $str;
+
+        $asc = ord($s{0}) * 256 + ord($s{1}) - 65536;
+
+        if ($asc >= - 20319 && $asc <= - 20284) return 'A';
+        if ($asc >= - 20283 && $asc <= - 19776) return 'B';
+        if ($asc >= - 19775 && $asc <= - 19219) return 'C';
+        if ($asc >= - 19218 && $asc <= - 18711) return 'D';
+        if ($asc >= - 18710 && $asc <= - 18527) return 'E';
+        if ($asc >= - 18526 && $asc <= - 18240) return 'F';
+        if ($asc >= - 18239 && $asc <= - 17923) return 'G';
+        if ($asc >= - 17922 && $asc <= - 17418) return 'H';
+        if ($asc >= - 17417 && $asc <= - 16475) return 'J';
+        if ($asc >= - 16474 && $asc <= - 16213) return 'K';
+        if ($asc >= - 16212 && $asc <= - 15641) return 'L';
+        if ($asc >= - 15640 && $asc <= - 15166) return 'M';
+        if ($asc >= - 15165 && $asc <= - 14923) return 'N';
+        if ($asc >= - 14922 && $asc <= - 14915) return 'O';
+        if ($asc >= - 14914 && $asc <= - 14631) return 'P';
+        if ($asc >= - 14630 && $asc <= - 14150) return 'Q';
+        if ($asc >= - 14149 && $asc <= - 14091) return 'R';
+        if ($asc >= - 14090 && $asc <= - 13319) return 'S';
+        if ($asc >= - 13318 && $asc <= - 12839) return 'T';
+        if ($asc >= - 12838 && $asc <= - 12557) return 'W';
+        if ($asc >= - 12556 && $asc <= - 11848) return 'X';
+        if ($asc >= - 11847 && $asc <= - 11056) return 'Y';
+        if ($asc >= - 11055 && $asc <= - 10247) return 'Z';
+
+        return null;
     }
 
     /**
@@ -226,7 +307,7 @@ class Pinyin
      *
      * @param string $dictionary cached file name
      *
-     * @return array 
+     * @return array
      */
     protected function loadFromCache($dictionary)
     {
@@ -261,9 +342,9 @@ class Pinyin
     /**
      * Credits for this function go to velcrow, who shared this
      * at http://stackoverflow.com/questions/1162491/alternative-to-mysql-real-escape-string-without-connecting-to-db
-     * 
+     *
      * @param string $string the string to  be escaped
-     * 
+     *
      * @return string the escaped string
      */
     protected function escape($value)
@@ -277,9 +358,9 @@ class Pinyin
     /**
      * Remove unwanted characters
      *
-     * @param string $string 
+     * @param string $string
      */
-    protected function removeUnwantedCharacters($string)    
+    protected function removeUnwantedCharacters($string)
     {
         $allowChars = ' a-zA-Z0-9āēīōǖǖĀĒĪŌŪǕáéíóǘǘÁÉÍÓÚǗǎěǐǒǚǚǍĚǏǑǓǙàèìòǜǜÀÈÌÒÙǛūúǔùüüÜ\p{Han}';
         $search = array(
@@ -292,11 +373,11 @@ class Pinyin
     /**
      * add delimiter
      *
-     * @param string $string 
+     * @param string $string
      */
-    protected function addDelimiter($string)    
+    protected function addDelimiter($string)
     {
-        return str_replace(array('  ', ' '), self::$setting['delimiter'], trim($string));
+        return str_replace(array('  ', ' '), $this->settings['delimiter'], trim($string));
     }
 
     /**
@@ -321,7 +402,7 @@ class Pinyin
      * at http://stackoverflow.com/questions/1598856/convert-numbered-to-accentuated-pinyin
      *
      * @param string $string The pinyin string with tone numbers, i.e. "ni3 hao3"
-     * 
+     *
      * @return string The formatted string with tone marks, i.e.
      */
     protected function pinyin_addaccents($string)
@@ -347,7 +428,7 @@ class Pinyin
                     'A*I A*O E*I IA* IA*O IE* IO* IU* ' .
                     'o*u ua* ua*i ue* ui* uo* üe* ' .
                     'O*U UA* UA*I UE* UI* UO* ÜE*';
-            $nostars = 
+            $nostars =
                     'a e i o u ü ' .
                     'A E I O U Ü ' .
                     'ai ao ei ia iao ie io iu ' .
@@ -359,9 +440,9 @@ class Pinyin
             $accentmap = array_combine(explode(' ', $nostars), explode(' ', $stars));
         }
 
-        static $vowels = array('a*', 'e*', 'i*', 'o*', 'u*', 'ü*', 'A*', 'E*', 'I*', 'O*', 'U*', 'Ü*');
+        $vowels = array('a*', 'e*', 'i*', 'o*', 'u*', 'ü*', 'A*', 'E*', 'I*', 'O*', 'U*', 'Ü*');
 
-        static $pinyin = array(
+        $pinyin = array(
             1 => array('ā', 'ē', 'ī', 'ō', 'ū',  'ǖ', 'Ā', 'Ē', 'Ī', 'Ō', 'Ū', 'Ǖ'),
             2 => array('á', 'é', 'í', 'ó', 'ú',  'ǘ', 'Á', 'É', 'Í', 'Ó', 'Ú', 'Ǘ'),
             3 => array('ǎ', 'ě', 'ǐ', 'ǒ', 'ǔ', 'ǚ', 'Ǎ', 'Ě', 'Ǐ', 'Ǒ', 'Ǔ', 'Ǚ'),
@@ -374,7 +455,7 @@ class Pinyin
         $word = strtr($word, $accentmap);
         # Replace starred letter with accented
         $word = str_replace($vowels, $pinyin[$tone], $word);
-        
+
         return $word;
     }
 
