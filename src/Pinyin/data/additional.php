bbf1056a062ff996890af7a3343f73c73abfbbca
安正超(Carlos) <anzhengchao@gmail.com>
2014-12-01 23:17:42 +0800

    【更新】补充错词

--- src/Pinyin/data/additional.php
+++ src/Pinyin/data/additional.php
@@ -4,4 +4,8 @@ return array(
         '了无生趣' => 'liao3 wu2 sheng1 qu4',
         '便宜' => 'pian2 yi2',
         '腊' => 'la4',
+        '龈' => 'yin2',
+        '万' => 'wan4',
+        '区' => 'qu1',
+        '区庄' => 'ou1 zhuang1',
        );
