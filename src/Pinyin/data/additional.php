c9276c8f11b864ab772fe210d9e9a10ae63f6ba3
Carlos - 安正超 <anzhengchao@gmail.com>
2015-01-08 19:32:06 +0800

    Update additional.php

--- src/Pinyin/data/additional.php
+++ src/Pinyin/data/additional.php
@@ -2,6 +2,7 @@
 //additional words
 return array(
         '了无生趣' => 'liao3 wu2 sheng1 qu4',
+        '直言贾祸' => 'zhi2 yan2 gu3 huo4'
         '便宜' => 'pian2 yi2',
         '腊' => 'la4',
         '龈' => 'yin2',
