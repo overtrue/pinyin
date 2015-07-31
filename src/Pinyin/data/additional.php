315c473b1a3761726bbcebe0efe736165c7f1833
wangyifu <wyfisgood@gmail.com>
2015-07-31 11:40:47 -0700

    Update additional.php
    add several new words

--- src/Pinyin/data/additional.php
+++ src/Pinyin/data/additional.php
@@ -4,7 +4,11 @@
 return array(
         '了无生趣' => 'liao3 wu2 sheng1 qu4',
         '直言贾祸' => 'zhi2 yan2 gu3 huo4',
-        '便宜' => 'pian2 yi2',
+        '少不更事' => 'shao4 bu4 geng1 shi4',
+	'三年五载' => 'san1 nian3 wu3 zai3',
+	'真真假假' => 'zhen1 zhen1 jia3 jia3',
+	'一哄而散' => 'yi2 hong4 er2 san4',
+        '便宜' => 'pian2 yi',
         '腊' => 'la4',
         '龈' => 'yin2',
         '万' => 'wan4',
@@ -13,4 +17,9 @@ return array(
         '鴜' => 'ci2',
         '鹭' => 'lu4',
         '俢' => 'xiu1',
+	'和' => 'he2',
+	'给' => 'gei3',
+	'大喝' => 'da4 he4',
+	'曝光' => 'bao4 guang1',
+	'教' => 'jiao1',
        );
