a613c570488ba47a83359b6c57e2966031cf8080
安正超 <anzhengchao@gmail.com>
2017-07-04 21:14:01 +0800

    Apply fixes from StyleCI (#94)

--- tests/FileToMemoryDictLoaderTest.php
+++ tests/FileToMemoryDictLoaderTest.php
@@ -3,7 +3,10 @@
 /*
  * This file is part of the overtrue/pinyin.
  *
- * @author Garveen <acabin@live.com>
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Overtrue\Pinyin\Test;
