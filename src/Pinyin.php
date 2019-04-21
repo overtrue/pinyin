d4b61ba8af4b5a907e918b5a40e7de7a0a48163b
silai <ljjgit@qq.com>
2019-04-21 21:18:00 +0800

    删除定义在类文件中的常量
    由于类是自动加载的，那么在自动加载之前常量存在，因为这个文件还未被php引擎读取

--- src/Pinyin.php
+++ src/Pinyin.php
@@ -23,16 +23,6 @@ use InvalidArgumentException;
  * @link      http://overtrue.me
  */
 
-define('PINYIN_DEFAULT', 4096);
-define('PINYIN_TONE', 2);
-define('PINYIN_NO_TONE', 4);
-define('PINYIN_ASCII_TONE', 8);
-define('PINYIN_NAME', 16);
-define('PINYIN_KEEP_NUMBER', 32);
-define('PINYIN_KEEP_ENGLISH', 64);
-define('PINYIN_UMLAUT_V', 128);
-define('PINYIN_KEEP_PUNCTUATION', 256);
-
 /**
  * Class Pinyin.
  *
