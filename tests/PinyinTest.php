5cff8c1f530c7cc12f8b274a23637a6892e78dcd
安正超 <anzhengchao@gmail.com>
2020-04-14 11:34:19 +0800

    Apply fixes from StyleCI (#155)

--- tests/PinyinTest.php
+++ tests/PinyinTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the overtrue/pinyin.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Overtrue\Pinyin\Test;
 
 use Closure;
